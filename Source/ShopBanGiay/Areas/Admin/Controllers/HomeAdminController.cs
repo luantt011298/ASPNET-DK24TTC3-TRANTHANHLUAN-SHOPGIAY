9652eee975f2b01d38c89524eeb4d6484f180b00
luantt011298 <luantt011298@tvu-onschool.edu.vn>
2025-12-08 13:01:34 +0700

    Tạo trang quản lý hãng sản xuất

--- Source/ShopBanGiay/Areas/Admin/Controllers/HomeAdminController.cs
+++ Source/ShopBanGiay/Areas/Admin/Controllers/HomeAdminController.cs
@@ -18,6 +18,8 @@ namespace ShopBanGiay.Areas.Admin.Controllers
         {          
             return View();
         }
+
+        //Xử lý tác vụ trên Bảng Danh mục sản phẩm
         [Route("danhmucsanpham")]
         public IActionResult DanhMucSP(int? page)
         {
@@ -30,7 +32,6 @@ namespace ShopBanGiay.Areas.Admin.Controllers
 
         [Route("themsanpham")]
         [HttpGet]
-
         public IActionResult ThemSanPham()
         {
             ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
@@ -55,7 +56,6 @@ namespace ShopBanGiay.Areas.Admin.Controllers
 
         [Route("suasanpham")]
         [HttpGet]
-
         public IActionResult SuaSanPham(string maSanPham)
         {
             ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
@@ -95,6 +95,76 @@ namespace ShopBanGiay.Areas.Admin.Controllers
             db.SaveChanges();
             TempData["Message"] = "Sản phẩm đã được xóa";
             return RedirectToAction("DanhMucSP", "HomeAdmin");
+        }
+
+        //Xử lý tác vụ trên Bảng Hãng sản xuất
+        [Route("danhmuchangsx")]
+        public IActionResult DanhMucHangSX(int? page)
+        {
+            int pageSize = 15;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lsthangsx = db.THangSxes.AsNoTracking().OrderBy(x => x.HangSx);
+            PagedList<THangSx> lsthang = new PagedList<THangSx>(lsthangsx, pageNumber, pageSize);
+            return View(lsthang);
+        }
+
+        [Route("themhangsx")]
+        [HttpGet]
+
+        public IActionResult ThemHangSX()
+        {
+            return View();
+        }
+        [Route("themhangsx")]
+        [HttpPost]
+        public IActionResult ThemHangSX(THangSx hang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.THangSxes.Add(hang);
+                db.SaveChanges();
+                return RedirectToAction("DanhMucHangSX");
+            }
+            return View(hang);
+        }
+
+        [Route("suahangsx")]
+        [HttpGet]
+
+        public IActionResult SuaHangSX(string maHangSX)
+        {
+            var hang = db.THangSxes.Find(maHangSX);
+            return View(hang);
+        }
+        [Route("suahangsx")]
+        [HttpPost]
+        public IActionResult SuaHangSX(THangSx hang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DanhMucHangSX", "HomeAdmin");
+            }
+            return View(hang);
+        }
+
+        [Route("xoahangsx")]
+        [HttpGet]
+        public IActionResult XoaHangSX(string maHangSX)
+        {
+            TempData["Message"] = "";
+            var hang = db.TDanhMucSps.Where(x => x.MaHangSx == maHangSX).ToList();
+            if (hang.Count() > 0)
+            {
+                TempData["Message"] = "Không được xóa hãng này";
+                return RedirectToAction("DanhMucHangSX", "HomeAdmin");
+
+            }
+            db.Remove(db.THangSxes.Find(maHangSX));
+            db.SaveChanges();
+            TempData["Message"] = "Hãng đã được xóa";
+            return RedirectToAction("DanhMucHangSX", "HomeAdmin");
         }
     }
 }
