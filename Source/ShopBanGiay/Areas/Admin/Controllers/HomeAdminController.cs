26039ed4c9947a3ccb4ffd95e0a566d410a328cd
luantt011298 <luantt011298@tvu-onschool.edu.vn>
2025-12-08 09:01:28 +0700

    Thêm chức năng tạo sản phẩm mới

--- Source/ShopBanGiay/Areas/Admin/Controllers/HomeAdminController.cs
+++ Source/ShopBanGiay/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ShopBanGiay.Models;
 using X.PagedList;
@@ -27,5 +28,29 @@ namespace ShopBanGiay.Areas.Admin.Controllers
             return View(lst);
         }
 
+        [Route("themsanpham")]
+        [HttpGet]
+
+        public IActionResult ThemSanPham()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaSd = new SelectList(db.TLoaiSds.ToList(), "MaSd", "TenLoai");
+            return View();
+        }
+        [Route("themsanpham")]
+        [HttpPost]
+        public IActionResult ThemSanPham(TDanhMucSp sanPham)
+        {
+            if(ModelState.IsValid)
+            {
+                db.TDanhMucSps.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSP");
+            }    
+            return View(sanPham);
+        }
     }
 }
